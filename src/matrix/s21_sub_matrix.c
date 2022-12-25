37cc9b7521531f10f1db9642057cefffe9ca7838
Lyub1m1y <dokhoyan-leva@mail.ru>
2022-12-25 15:46:19 +0400

    added func s21_sub_matrix and test to it

--- src/matrix/s21_sub_matrix.c
+++ src/matrix/s21_sub_matrix.c
@@ -1,11 +1,23 @@
-// #include "../s21_matrix.h"
+#include "../s21_matrix.h"
+#include "common.h"
 
-// int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
-//   int status = 0;
-//   //   if () {
-//   //     status = OK;
-//   //   } else {
-//   //     status = ERROR;
-//   //   }
-//   return status;
-// }
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+  int status = OK;
+  if (s21_is_correct(A) && s21_is_correct(B)) {
+    if (A->rows == B->rows && A->columns == B->columns) {
+      status = s21_create_matrix(A->rows, A->columns, result);
+      if (status != ERROR) {
+        for (int i = 0; i < A->rows; i++) {
+          for (int j = 0; j < A->columns; j++) {
+            result->matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
+          }
+        }
+      }
+    } else {
+      status = ERROR_CALC;
+    }
+  } else {
+    status = ERROR;
+  }
+  return status;
+}
--- src/tests/list.h
+++ src/tests/list.h
@@ -7,6 +7,7 @@ TCase *tcase_matrix_s21_create_matrix();
 TCase *tcase_matrix_s21_remove_matrix();
 TCase *tcase_matrix_s21_eq_matrix();
 TCase *tcase_matrix_s21_sum_matrix();
+TCase *tcase_matrix_s21_sub_matrix();
 
 #define suite_add_tcase_list(suite)                             \
   do {                                                          \
@@ -14,6 +15,7 @@ TCase *tcase_matrix_s21_sum_matrix();
     suite_add_tcase((suite), tcase_matrix_s21_remove_matrix()); \
     suite_add_tcase((suite), tcase_matrix_s21_eq_matrix());     \
     suite_add_tcase((suite), tcase_matrix_s21_sum_matrix());    \
+    suite_add_tcase((suite), tcase_matrix_s21_sub_matrix());    \
   } while (0)
 
 #endif  // SRC_TESTS_LIST_H_
